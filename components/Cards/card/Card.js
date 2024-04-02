1331a29c4d1dc393e72e54325a7098f0bf47eb4d
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-03 02:07:49 +0500

    Update

--- components/Cards/card/Card.js
+++ components/Cards/card/Card.js
@@ -34,9 +34,34 @@ const Card = () => {
             Make your app launch a success with tools and strategies to help
             publish, manage, and distribute your app worldwide
           </div>
-          <div className={style.buttons}>
-            <a href="#"> Learn more </a>
+          <div className={style.wrap}>
+            <div className={style.review}>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 576 512"
+                color="orange"
+                height="12"
+                width="12"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ color: "orange" }}
+              >
+                <path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path>
+              </svg>
+              <span className="text-sm mt-[1px]">
+                5.0
+                <span style={{ color: "silver", margin: "0 4px" }}>|</span>1
+                review
+              </span>
+            </div>
+            <div className={style.review_text}>
+              <span className="text-sm">Productivity</span>
+            </div>
           </div>
+          {/* <div className={style.buttons}>
+            <a href="#"> Learn more </a>
+          </div> */}
         </div>
       </div>
     </div>
--- components/smallCards/smallCard/SmallCard.js
+++ components/smallCards/smallCard/SmallCard.js
@@ -1,7 +1,7 @@
 import { TfiHelpAlt } from "react-icons/tfi";
 import { SiHomeassistantcommunitystore } from "react-icons/si";
 import { BsFillPeopleFill } from "react-icons/bs";
-
+import { IoIosList } from "react-icons/io";
 import Link from "next/link";
 import React from "react";
 import style from "./smallCard.module.scss";
@@ -23,72 +23,168 @@ const SmallCard = () => {
         <li>
           <Link href="#" className="bg-[#e5dbf6]">
             <h3>Why Andriod</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <TfiHelpAlt size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e8f0fe]">
             <h3>Solutions</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <SiHomeassistantcommunitystore size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#d1daf4]">
             <h3>Industry</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <BsFillPeopleFill size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e5dbf6]">
             <h3>Why Andriod</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <TfiHelpAlt size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e8f0fe]">
             <h3>Solutions</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <SiHomeassistantcommunitystore size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#d1daf4]">
             <h3>Industry</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <BsFillPeopleFill size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e5dbf6]">
             <h3>Why Andriod</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <TfiHelpAlt size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e8f0fe]">
             <h3>Solutions</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <SiHomeassistantcommunitystore size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#d1daf4]">
             <h3>Industry</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <BsFillPeopleFill size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e5dbf6]">
             <h3>Why Andriod</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <TfiHelpAlt size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#e8f0fe]">
             <h3>Solutions</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <SiHomeassistantcommunitystore size={36} className={style.icon} />
           </Link>
         </li>
         <li>
           <Link href="#" className="bg-[#d1daf4]">
             <h3>Industry</h3>
+            <div className=" my-[10px]">
+              <span className="flex gap-1  items-center">
+                <IoIosList color="#666" />
+                <div href="#" className="underline text-[#999] text-sm">
+                  256 altenative
+                </div>
+              </span>
+            </div>
             <BsFillPeopleFill size={36} className={style.icon} />
           </Link>
         </li>
