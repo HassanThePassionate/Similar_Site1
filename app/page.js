1794ca9daa83d8014729abf9f2a00273e961e02a
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-05 00:22:15 +0500

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Cards from "@/components/Cards/Cards";
 import GameCards from "@/components/gameCards/GameCards";
+import HeroSearch from "@/components/heroSearch/HeroSearch";
 import SmallCard from "@/components/smallCards/smallCard/SmallCard";
 import SimilarCard from "@/components/thumbCards/SimilarCard/SimilarCard";
 const page = () => {
   return (
     <div>
+      <HeroSearch />
       <Cards />
       <SmallCard />
       <SimilarCard />
